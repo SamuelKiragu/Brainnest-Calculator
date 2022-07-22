bf85070f87bcfb0ebb6795d6e7669c7cbd30e428
Kiragu <samuelkiragu107@gmail.com>
2022-07-22 21:36:48 +0300

    added HTML BoilerPlate added add() added subtract() added multiply() added divide() added operate()

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+//adds two integers
+function add(a,b){
+    return a + b;
+} 
+
+//subtracts two integers
+function subtract(a,b){
+    return a - b;
+}
+
+//multiply two integers
+function multiply(a,b){
+    return a * b;
+}
+
+//divide two integers
+function divide(a,b){
+    return a / b;
+}
+
+//takes operator and two integers and calls any of the functions above
+function operate(op,a,b){
+    const OPERATORS = ["+", "-", "*", "/"];
+    const OPERATIONS = [add,subtract,multiply,divide];
+    return OPERATIONS[OPERATORS.indexOf(op)](a,b);
+}
+
